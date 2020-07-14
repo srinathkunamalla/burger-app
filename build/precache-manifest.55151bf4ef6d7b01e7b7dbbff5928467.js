99fd0b1271c5c919be08efe6fc669e02a1b705bf
Srinath Kunamalla <s0k022y@homeoffice.wal-mart.com>
2020-07-14 00:53:16 -0500

    new chnages

---
+++ build/precache-manifest.55151bf4ef6d7b01e7b7dbbff5928467.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35045db4359becc88604be9b80c6d760",
+    "url": "/burger-app/index.html"
+  },
+  {
+    "revision": "dc34413746b303c1ca7f",
+    "url": "/burger-app/static/css/main.390326b0.chunk.css"
+  },
+  {
+    "revision": "dc58842ea951f80857b4",
+    "url": "/burger-app/static/js/2.009691de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burger-app/static/js/2.009691de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc34413746b303c1ca7f",
+    "url": "/burger-app/static/js/main.4850f70f.chunk.js"
+  },
+  {
+    "revision": "2d2a941fd68d60f80796",
+    "url": "/burger-app/static/js/runtime-main.6b8b2eb0.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-app/static/media/burger-logo.b8503d26.png"
+  }
+]);
